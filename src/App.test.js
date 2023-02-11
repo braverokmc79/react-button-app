5994e1de163af08bf985e7bcdbe9b2223766ad02
unknown <braverokmc79@gmail.com>
2023-02-11 22:33:09 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter start 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement =screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
 });
+
+test('minus button has correct text',  ()=>{
+  render(<App /> );
+  const buttontElement =screen.getByTestId("minus-button");
+  expect(buttontElement).toHaveTextContent("-")
+});
+
+
+test('plus button has correct text', ()=>{
+  render(<App />);
+  const buttontElement =screen.getByTestId("plus-button");
+  expect(buttontElement).toHaveTextContent("+");
+})

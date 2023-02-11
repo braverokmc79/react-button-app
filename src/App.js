e909232650460c6782e1cc16f96c8f8763bdd0d3
unknown <braverokmc79@gmail.com>
2023-02-11 22:45:57 +0900

    69.플러스, 마이너스 버튼 기능 넣기(fire event)

--- src/App.js
+++ src/App.js
@@ -8,14 +8,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{count}</h3>
-        <button data-testid="minus-button">-</button>
-        <button data-testid="plus-button">+</button>
+        <button data-testid="minus-button" onClick={()=>setCount((count)=>count-1)}>-</button>
+        <button data-testid="plus-button" onClick={()=>setCount((count)=>count+1)}>+</button>
       </header>
     </div>
   );
 }
-
-
-
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('the counter start 0', () => {
@@ -18,4 +18,17 @@ test('plus button has correct text', ()=>{
   render(<App />);
   const buttontElement =screen.getByTestId("plus-button");
   expect(buttontElement).toHaveTextContent("+");
-})
+});
+
+
+
+test('When the + button is pressed, the counter changes to 1', ()=>{
+  render(<App />);
+  const buttontElement =screen.getByTestId("plus-button");
+  fireEvent.click(buttontElement);
+
+  const counterElement =screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+});
+
+
